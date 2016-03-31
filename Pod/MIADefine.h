09af3834f79a7bb8a4db060ec6a49873b5ebd8dd
杨鹏 <919548782@qq.com>
2016-03-31 13:20:22 +0800

    add singleton

--- Pod/MIADefine.h
+++ Pod/MIADefine.h
@@ -186,3 +186,70 @@ cls *inst_ = (cls *)(x);                        \
 #define IsNilOrNull(_ref)   (((_ref) == nil) || ([(_ref) isEqual:[NSNull null]]) || ([[_ref stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] length]==0))
 
 #endif /* FIUserDef_h */
+
+// .h文件
+#define HMSingletonH(name) + (instancetype)shared##name;
+
+// .m文件
+#if __has_feature(objc_arc)
+
+#define HMSingletonM(name) \
+static id _instace; \
+\
++ (id)allocWithZone:(struct _NSZone *)zone \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instace = [super allocWithZone:zone]; \
+}); \
+return _instace; \
+} \
+\
++ (instancetype)shared##name \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instace = [[self alloc] init]; \
+}); \
+return _instace; \
+} \
+\
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return _instace; \
+}
+
+#else
+
+#define HMSingletonM(name) \
+static id _instace; \
+\
++ (id)allocWithZone:(struct _NSZone *)zone \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instace = [super allocWithZone:zone]; \
+}); \
+return _instace; \
+} \
+\
++ (instancetype)shared##name \
+{ \
+static dispatch_once_t onceToken; \
+dispatch_once(&onceToken, ^{ \
+_instace = [[self alloc] init]; \
+}); \
+return _instace; \
+} \
+\
+- (id)copyWithZone:(NSZone *)zone \
+{ \
+return _instace; \
+} \
+\
+- (oneway void)release { } \
+- (id)retain { return self; } \
+- (NSUInteger)retainCount { return 1;} \
+- (id)autorelease { return self;}
+
+#endif
